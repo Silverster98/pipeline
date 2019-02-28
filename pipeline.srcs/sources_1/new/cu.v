0fd500ee0a8e0648888d84ffe129200d92c3d3f9
Silvester98 <1539168414@qq.com>
2019-02-28 20:31:05 +0800

    add part of cu code

---
+++ pipeline.srcs/sources_1/new/cu.v
@@ -0,0 +1,120 @@
+`timescale 1ns / 1ps
+`include "instruction.v"
+
+module cu(
+    input wire[5:0] op,
+    input wire[5:0] funct,
+    
+    output reg reg_wen, mem_wen, branch,
+    output reg[2:0] aluctrl,
+    output reg sel_reg_wdata, sel_srcB, sel_regdst
+    );
+    
+    wire Rtype;
+    assign Rtype = (op == `INST_TYPE_R) ? 1 : 0;
+    
+    always @ (*) begin
+        if (Rtype) begin
+            case (funct)
+            `INST_ADD : begin
+                reg_wen    = 1;
+                mem_wen    = 0;
+                branch     = 0;
+                aluctrl    = `ALU_ADD;
+                sel_srcB   = 0;
+                sel_regdst = 1;
+                sel_reg_wdata = 0;
+            end
+            `INST_SUB : begin
+                reg_wen    = 1;
+                mem_wen    = 0;
+                branch     = 0;
+                aluctrl    = `ALU_SUB;
+                sel_srcB   = 0;
+                sel_regdst = 1;
+                sel_reg_wdata = 0;
+            end
+            `INST_SLT : ;
+            `INST_OR  : begin
+                reg_wen    = 1;
+                mem_wen    = 0;
+                branch     = 0;
+                aluctrl    = `ALU_OR;
+                sel_srcB   = 0;
+                sel_regdst = 1;
+                sel_reg_wdata = 0;
+            end
+            `INST_AND : begin
+                reg_wen    = 1;
+                mem_wen    = 0;
+                branch     = 0;
+                aluctrl    = `ALU_AND;
+                sel_srcB   = 0;
+                sel_regdst = 1;
+                sel_reg_wdata = 0;
+            end
+            `INST_SLL : begin
+                reg_wen    = 1;
+                mem_wen    = 0;
+                branch     = 0;
+                aluctrl    = `ALU_SL;
+                sel_srcB   = 0;
+                sel_regdst = 1;
+                sel_reg_wdata = 0;
+            end
+            `INST_SRL : begin
+                reg_wen    = 1;
+                mem_wen    = 0;
+                branch     = 0;
+                aluctrl    = `ALU_ADD;
+                sel_srcB   = 0;
+                sel_regdst = 1;
+                sel_reg_wdata = 0;
+            end
+            endcase
+        end else begin
+            case (op)
+            `INST_ADDIU : begin
+                reg_wen    = 1;
+                mem_wen    = 0;
+                branch     = 0;
+                aluctrl    = `ALU_ADD;
+                sel_srcB   = 1;
+                sel_regdst = 0;
+                sel_reg_wdata = 0;
+            end
+            `INST_ADDI  : begin
+                reg_wen    = 1;
+                mem_wen    = 0;
+                branch     = 0;
+                aluctrl    = `ALU_ADD;
+                sel_srcB   = 1;
+                sel_regdst = 0;
+                sel_reg_wdata = 0;
+            end
+            `INST_ORI   : begin
+                reg_wen    = 1;
+                mem_wen    = 0;
+                branch     = 0;
+                aluctrl    = `ALU_OR;
+                sel_srcB   = 1;
+                sel_regdst = 0;
+                sel_reg_wdata = 0;
+            end
+            `INST_BEQ   : begin
+                reg_wen    = 0;
+                mem_wen    = 0;
+                branch     = 1;
+                aluctrl    = `ALU_SUB;
+                sel_srcB   = 0;
+                sel_regdst = 0;
+                sel_reg_wdata = 0;
+            end
+            `INST_LW    : ;
+            `INST_SW    : ;
+            `INST_J     : ;
+            `INST_JAL   : ;
+            endcase
+        end
+    end
+endmodule
